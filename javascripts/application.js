56b82ea2da737fb8eacf7ea1337ec476f96a3ad3
Colin Stein <colinstein@me.com>
2014-12-29 23:50:32 -0700

    stubbed in playlist links

--- javascripts/application.js
+++ javascripts/application.js
@@ -41,6 +41,41 @@ $(document).ready(function() {
   })();
 
 
+  /* --------------------------------------------------------------------- 
+     Portfolio
+     ------------------------------------------------------------------------ */
+  (function() {
+    $('.tp-grid').addClass('initial');
+    var $grid = $( '#tp-grid' ),
+    $name = $( '#p-name' ),
+      $ptitle  = $('#p-name').html(),
+      $close = $( '#close' ),
+        $loader = $( '<div class="loader"><i></i><i></i><i></i><i></i><i></i><i></i><span>Loading...</span></div>' ).insertBefore( $grid ),
+
+        stapel = $grid.stapel({
+          onLoad : function() {
+            $loader.remove();
+          },
+          onBeforeOpen : function( pileName ){
+            $name.html( 'Videos for playlist: ' + pileName );
+            $('.tp-grid').removeClass('initial');
+
+          },
+          onAfterOpen : function( pileName ){
+            $close.show();
+          }, 
+        });
+
+    $close.on( 'click', function(){
+      $close.hide();
+      $name.html($ptitle);
+      stapel.closePile();
+      $('.tp-grid').addClass('initial');
+    });
+  })();
+
+
+
   /* ---------------------------------------------------------------------
      Page Scrolling Animation
      ------------------------------------------------------------------------ */
